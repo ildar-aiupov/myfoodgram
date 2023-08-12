4bfe9dbca5fba04b1198e01021a074dd519a1aeb
Ильдар Аюпов <ildarbon@gmail.com>
2023-08-12 17:30:16 +0500

    fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,8 +62,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB", "foodgram"),
-        "USER": os.getenv("POSTGRES_USER", "foodgram_user"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "foodgram_password"),
+        "USER": os.getenv("POSTGRES_USER", "foodgram"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "foodgram"),
         "HOST": os.getenv("DB_HOST", "db"),
         "PORT": os.getenv("DB_PORT", 5432),
     }
